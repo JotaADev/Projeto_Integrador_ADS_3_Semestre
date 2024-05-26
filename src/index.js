a1c96d8f19c417db8a1cc317dfcfae7f1e47a830
Jonas Almeida <126114565+JotaADev@users.noreply.github.com>
2024-05-25 21:34:48 -0300

    Correção do router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/AppRoutes";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// =========== Páginas importadas ===========
+import Home from "./pages/Home/Home";
+import PanelAdministratorLogin from "./pages/PanelAdministratorLogin/PanelAdministratorLogin";
+import PanelAdministrator from "./pages/PanelAdministrator/PanelAdministrator";
+
+//// =========== Componente de rotas ===========
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home/>,
+    },
+    {
+        path: 'admin',
+        element: <PanelAdministratorLogin/>,
+    },
+    {
+        path: 'panel-admin',
+        element: <PanelAdministrator/>,
+    },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
