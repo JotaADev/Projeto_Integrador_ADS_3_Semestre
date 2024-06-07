f682cb7251a3f0c98bcda3cd98546fb0bffb1d12
JotaADev <jonasalmeidareis@gmail.com>
2024-06-06 22:31:01 -0300

    Correção da exibição do produto e adição da funcionalidade da barra de pesquisa

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,12 +4,100 @@ import './SearchBar.css';
 // =========== Icones importados ===========
 import { CiSearch } from "react-icons/ci";
 
+// =========== Componentes importados ===========
+import ProductModal from '../ProductModal/ProductModal';
+
+// =========== Funções importadas ===========
+import getProducts from '../../firebase/getProducts';
+
 const SearchBar = () => {
+
+    const [inputValue, setInputValue] = React.useState('');
+    const [products, setProducts] = React.useState([]);
+    const [product, setProduct] = React.useState([]);
+    const [toggle, setToggle] = React.useState(false);
+
+    React.useEffect(() => {
+        const searchProducts = async () => {
+            if(inputValue) {
+            
+                const queryWait = await getProducts('nome_produto');
+
+                const productInfo = queryWait.docs.map(doc => ({
+                    id: doc.id,
+                    img: doc.data().imagem_produto,
+                    product: doc.data().nome_produto,
+                    old_price: doc.data().preco_anterior,
+                    price: doc.data().preco_atual
+                }));
+        
+                const filteredResults = productInfo.filter(product => 
+                    product.product.includes(inputValue)
+                );
+        
+                setProducts(filteredResults);
+            } else {
+                setProducts([]);
+            }
+        }
+
+        searchProducts();
+    }, [inputValue]);
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const fetchProductInfo = async (productName) => {
+        const queryWait = await getProducts('nome_produto', '==', productName);
+        const productInfo = queryWait.docs.map(doc => ({
+            id: doc.id,
+            img: doc.data().imagem_produto,
+            product: doc.data().nome_produto,
+            old_price: doc.data().preco_anterior,
+            price: doc.data().preco_atual
+        }));
+
+        setProduct(productInfo);
+        setToggle(!toggle);
+    }
+
     return (
-        <div className='searchbar'>
-            <input className='searchinput' placeholder='Qual produto você está procurando?' name='searchbar'/>
-            <CiSearch size={'20px'} fontWeight={'bold'}/>
-        </div>
+        <>
+            <div className='containersearchbar'>
+                <div className='searchbar'>
+                    <input className='searchinput'
+                        placeholder='Qual produto você está procurando?'
+                        name='searchbar'
+                        value={inputValue}
+                        onChange={handleInputChange}
+                    />
+                    <CiSearch size={'20px'} fontWeight={'bold'}/>
+                </div>
+                <div className='results'>
+                    {products.map((product) => (
+                        <div key={product.id} onClick={() => {fetchProductInfo(product.product)}}>
+                            <img src={product.img}/>
+                            <p>{product.product}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            {
+                product.map((product) => (
+                    <ProductModal
+                        key={product.id}
+                        img={product.img}
+                        productName={product.product}
+                        oldPrice={product.old_price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                        price={product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                        payment={(product.price / 3).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                        classStyle={toggle ? 'show' : 'close'}
+                        manipToggle={() => setToggle(!toggle)}
+                    />
+                ))
+            }
+        </>
     );
 }
 
--- src/Pages/ProductsByCategory/ProductsByCategory.js
+++ src/Pages/ProductsByCategory/ProductsByCategory.js
@@ -10,6 +10,7 @@ import Header from '../../Components/Header/Header';
 import NavBar from '../../Components/NavBar/NavBar';
 import ProductCard from '../../Components/ProductCard/ProductCard';
 import Footer from '../../Components/Footer/Footer';
+import ProductModal from "../../Components/ProductModal/ProductModal";
 
 // =========== Funções importadas ===========
 import getProducts from '../../firebase/getProducts';
@@ -19,6 +20,22 @@ const ProductsByCategory = () => {
     const { categoria } = useParams();
 
     const [products, setProducts] = React.useState([]);
+    const [product, setProduct] = React.useState([]);
+    const [toggle, setToggle] = React.useState(false);
+
+    const fetchProductInfo = async (productName) => {
+        const queryWait = await getProducts('nome_produto', '==', productName);
+        const productInfo = queryWait.docs.map(doc => ({
+            id: doc.id,
+            img: doc.data().imagem_produto,
+            product: doc.data().nome_produto,
+            old_price: doc.data().preco_anterior,
+            price: doc.data().preco_atual
+        }));
+
+        setProduct(productInfo);
+        setToggle(!toggle);
+    }
 
     React.useEffect(() => {
         const fetchProducts = async () => {
@@ -56,10 +73,25 @@ const ProductsByCategory = () => {
                                 oldPrice={product.old_price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
                                 price={product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
                                 payment={(product.price / 3).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                                propFunction={() => {fetchProductInfo(product.product)}}
                             />
                         ))
                     }
                 </div>
+                {
+                    product.map((product) => (
+                        <ProductModal
+                            key={product.id}
+                            img={product.img}
+                            productName={product.product}
+                            oldPrice={product.old_price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                            price={product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                            payment={(product.price / 3).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                            classStyle={toggle ? 'show' : 'close'}
+                            manipToggle={() => setToggle(!toggle)}
+                        />
+                    ))
+                }
                 <Footer/>
             </main>
         </>
